df6762022eededf52bfa019ac6d013851042af98
Peter Luptak <peterluptak@MacBook-Air.local>
2017-07-09 01:02:05 +0200

    Initial commit - laying out the structure

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,35 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp, double Ki, double Kd) 
+{
+	Kp = Kp;
+	Ki = Ki;
+	Kd = Kd;
+
+	p_error = -100; // init value
+	i_error = -100; // init value
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(double cte)
+{
+	
+	// Check if we already have a valid initialize p_error
+	if(p_error == -100)
+	{
+		p_error = cte;
+	}
+
+	d_error = cte - p_error;
+
+
+
+
+
 }
 
-double PID::TotalError() {
+double PID::TotalError() 
+{
+	return i_error += cte; 
 }
 
